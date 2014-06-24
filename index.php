d833a9a1257e132ca5ea38bbc8fd1c68afda7556
Ambrus László <dr.ambrus.laszlo@gmail.com>
2014-06-25 01:28:15 +0200

    Definiing the Task

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
     /* Check if git commit is working to my repo:) {from php storm} */
+
+    /*
+     * Write a php script that will find
+     * the sum of all the multiples of 3 or 5 below 1000.
+     * The script should run from command line and put the result on screen.
+    */
+
+
 ?>
