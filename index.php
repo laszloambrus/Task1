059e0b362880cc832dfcff0bae056c1cfb8a4ebc
Ambrus László <dr.ambrus.laszlo@gmail.com>
2014-06-25 02:56:48 +0200

    The solution for the 1st task. There are some small cosmetic issues with reading from the command line as fgets reads EOF as well.

--- index.php
+++ index.php
@@ -7,5 +7,56 @@
      * The script should run from command line and put the result on screen.
     */
 
+    //Just a test for command line - (I've added PHP path to my windows system variables.)
+    //echo "this line is on cmd window";
 
+    /*===TODO
+     * 1. Read inputs from command line
+     * 2. Store them in variables
+     * 3. Pass them to a function as parameters
+     * 4. Output the result on screen.
+     * */
+
+    /*
+     * my Solution: The multiples of 3 is 3,6,9 and so on,
+     * so if I divide 6/3 the result will be 2 and the MOD% (or) rest is 0
+     * the same for 5...
+     * Because the task says 3 "or" 5 it means I have to
+     * check either One or the Other equals 0 using the or "||" statement
+     * I have to check the numbers up to 1000. So i make a "for" statement
+     * incrementing the values by One starting the index from 0.
+     */
+
+    echo "Task 1 - Sum of Multiples.\n";
+
+    //Fetching data from input - Trimming any spaces and storing in variable
+    $num1 = '';
+    echo "\nPlease enter a number below 1000!";
+    while(!$num1){
+        $num1 = trim(fgets(STDIN,3));
+    }
+
+    $num2 = '';
+    echo "\nPlease enter the second number!";
+    while(!$num2){
+        $num2 = trim(fgets(STDIN,3));
+    }
+
+    //Echoing the sum
+    echo "The sum of ".$num1." or ".$num2." multiples is: ".sumOfMultiples($num1,$num2);
+
+    //Function that does the math
+    function sumOfMultiples($num1,$num2){
+        //Initializing sum
+        $sum = 0;
+
+        //Numbers less than 1000
+        for($i = 0;$i<1000; $i++){
+            //MOD is 0 at either numbers?
+            if($i%$num1===0 || $i%$num2===0){
+                $sum = $sum + $i;
+            }
+        }
+        return $sum;
+    }
 ?>
